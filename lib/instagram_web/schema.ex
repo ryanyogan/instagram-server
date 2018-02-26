270d13c0858c0e215c35d4f1c68d23a6067dcad5
Ryan Yogan <ryanyogan@gmail.com>
2018-02-25 22:10:50 -0600

    Added in oauth -> facebook auth

--- lib/instagram_web/schema.ex
+++ lib/instagram_web/schema.ex
@@ -4,6 +4,7 @@ defmodule InstagramWeb.Schema do
   alias InstagramWeb.Resolvers
 
   import_types __MODULE__.PostsTypes
+  import_types __MODULE__.AccountsTypes
 
   query do
     @desc "Retreive a list of photos"
@@ -15,6 +16,15 @@ defmodule InstagramWeb.Schema do
     field :photo, :photo do
       arg :id, non_null(:id)
       resolve &Resolvers.Posts.photo/3
+    end
+  end
+
+  mutation do
+    @desc "Login via Facebook"
+    field :login, :user_session do
+      arg :token, :string
+      arg :provider, type: :provider
+      resolve &Resolvers.Accounts.login/3
     end
   end
 end
